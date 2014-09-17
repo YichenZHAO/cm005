b711bb6e441a1effe49723936457eef985086747
YichenZHAO <zhaoyc86@hotmail.com>
2014-09-17 09:54:57 -0700

    cm005_Sep17

--- cm005.R
+++ cm005.R
@@ -1,4 +1,12 @@
 library(lattice)
 
-install.packages("ggplot2", dependencies = TRUE)
+install.packages("ggplot2", dependencies = TRUE) 
 library(ggplot2)
+
+#lattice: 
+  #multi-panel conditioning
+  #
+
+#ggplot2: 
+  #facetting 
+  #aesthetic mapping <aes>
